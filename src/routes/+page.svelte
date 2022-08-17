b4a9e9f499332f4b60eb81d5e1172ce89464b7b5
odj0220 <odj0220@nate.com>
2022-08-18 00:53:12 +0900

    메인 페이지 완료

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let name = '';
+    function handleSubmit() {
+        if (name) {
+            location.href = `/summoners/${name}`;
+        }
+    }
+</script>
+
+<section class="main-img">
+    <img src="https://opgg-static.akamaized.net/logo/20220815090917.b3aca19a45e54ad889f8a8ed9cf1dd2d.png?image=q_auto,f_webp,w_auto&amp;v=1660730703413" alt="OP.GG logo (아무무)" title="아무무">
+</section>
+<section class="search-contaienr">
+    <form on:submit|preventDefault={handleSubmit}>
+        <label for="searchSummoner" class="hidden">소환사명, 소환사명, ...</label>
+        <input type="text" id="searchSummoner" name="searchSummoner" bind:value={name} placeholder="소환사명, 소환사명, ...">
+        <button type="submit">
+            <img src="https://s-lol-web.op.gg/images/icon/icon-gg-white.svg?v=1660730703413" width="30" alt="search" height="15">
+        </button>
+    </form>
+</section>
+
+<style lang="scss">
+    .main-img {
+      padding: 32px 0 24px;
+      img {
+        display: block;
+        max-height: 200px;
+        margin: 0 auto;
+      }
+    }
+    .search-contaienr {
+      position: relative;
+      border-radius: 2px;
+      background-color: #fff;
+      width: 624px;
+      margin: 0 auto;
+
+      input {
+        width: 100%;
+        padding: 15px 150px 18px 17px;
+        border: 0;
+        height: 50px;
+        box-sizing: border-box;
+        box-shadow: 0 2px 2px 0 rgb(0 0 0 / 19%);
+        border-radius: 4px;
+        font-size: 14px;
+        color: #9b9b9b;
+        outline: none;
+      }
+      button {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        width: 50px;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #01a9ff;
+        border-radius: 2px;
+        border: 0;
+        padding: 0;
+        cursor: pointer;
+      }
+    }
+    .hidden {
+      clip: rect(0 0 0 0);
+      width: 1px;
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      position: absolute;
+    }
+</style>
---
+++ src/routes/summoners/[name]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+
+    let name = $page.params.name;
+</script>
+
+<section>
+    {name}
+</section>
