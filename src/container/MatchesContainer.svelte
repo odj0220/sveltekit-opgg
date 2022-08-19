70ae9b1868f0581a4830cfd655a4e6097eb4e9be
odj0220 <odj0220@nate.com>
2022-08-19 14:40:08 +0900

    컨텐츠 메인에 위치할 MatchesContainer 추가

---
+++ src/container/MatchesContainer.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+
+    export let matches;
+    type GameTypes = 'TOTAL' | 'SOLORANKED' | 'FLEXRANKED';
+    let selectedType: GameTypes = 'SOLORANKED';
+
+    const selectType = (type: GameTypes) => {
+        selectedType = type;
+    }
+
+</script>
+
+<style lang="scss">
+  .container-header {
+    border: 1px solid #cdd2d2;
+    box-shadow: 0 1px #dcdfdf;
+    background: #f2f2f2;
+    border-radius: 2px;
+    position: relative;
+    border-bottom: 1px solid #cdd2d2;
+    margin-bottom: -1px;
+    height: 34px;
+
+    ul {
+      float: left;
+      padding-left: 10px;
+
+      li {
+        display: inline-block;
+        height: 32px;
+        margin: 0 10px;
+        vertical-align: top;
+        color: #555e5e;
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 36px;
+        text-decoration: none;
+        cursor: pointer;
+
+        &.active {
+          border-bottom: 2px solid #1f8ecd;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+</style>
+
+<div class="container-wrap">
+    <div class="container-header">
+        <ul>
+            <li class:active={selectedType === 'TOTAL'} on:click={() => selectType('TOTAL')}>전체</li>
+            <li class:active={selectedType === 'SOLORANKED'} on:click={() => selectType('SOLORANKED')}>솔로랭크</li>
+            <li class:active={selectedType === 'FLEXRANKED'} on:click={() => selectType('FLEXRANKED')}>자유랭크</li>
+        </ul>
+    </div>
+
+</div>
